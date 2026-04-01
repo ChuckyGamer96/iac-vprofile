3e110da44bae6cb15971b197f85a34363ccabc86
chuckygamer96 <ahmedkhan14@live.com>
2026-04-01 20:33:10 +0100

    Safe 11

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -7,7 +7,7 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access = false
   tags = {
     Environment = "dev1"
   }
