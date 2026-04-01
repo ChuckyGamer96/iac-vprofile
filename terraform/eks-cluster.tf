ba9fdfba6cad175a292e25cb1f6cf0da48770af8
chuckygamer96 <ahmedkhan14@live.com>
2026-04-01 20:20:49 +0100

    Risky 8

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,7 +9,7 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = false
   tags = {
-    Environment = "prod"
+    Environment = "test2"
   }
 
   eks_managed_node_group_defaults = {
