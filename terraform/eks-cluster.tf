22f9b6ce7ec53d195714b12f83208792de059123
chuckygamer96 <ahmedkhan14@live.com>
2026-04-20 18:24:40 +0100

    Safe 39

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,7 +9,7 @@ module "eks" {
   subnet_ids                                   = module.vpc.public_subnets
   cluster_endpoint_public_access               = true
   node_security_group_enable_recommended_rules = true
-  cluster_endpoint_public_access_cidrs         = ["0.0.0.0/0"]
+  cluster_endpoint_public_access_cidrs         = ["10.0.0.0/16"]
   cluster_enabled_log_types                    = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   tags = {
     Environment = "2node-risk-1"
