ff8cd2b44983b7e0bbd2877076ef01c3162ceb75
chuckygamer96 <ahmedkhan14@live.com>
2026-04-20 18:37:45 +0100

    Risky 42

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,10 +8,10 @@ module "vpc" {
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.4.0/24"]
+  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
   enable_dns_hostnames = true
 
   public_subnet_tags = {
