a5881b3476a823d312db85963dd504ce46e3d01c
chuckygamer96 <ahmedkhan14@live.com>
2026-04-20 22:09:27 +0100

    Risky 54

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,16 +9,16 @@ module "eks" {
   subnet_ids                                   = module.vpc.public_subnets
   cluster_endpoint_public_access               = true
   node_security_group_enable_recommended_rules = false
-  cluster_endpoint_public_access_cidrs         = ["172.16.0.0/12"]
+  cluster_endpoint_public_access_cidrs         = ["0.0.0.0/0"]
   cluster_enabled_log_types                    = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   node_security_group_additional_rules = {
     egress_all_custom = {
-      description = "Custom allow all egres"
-      protocol    = "-1"
-      from_port   = 0
-      to_port     = 0
+      description = "restricted egres"
+      protocol    = "tcp"
+      from_port   = 443
+      to_port     = 443
       type        = "egress"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["172.20.0.0/16"]
     }
   }
   tags = {
